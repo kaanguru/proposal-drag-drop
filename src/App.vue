7afe11a5d62950fe2548378224fb6802e2e3f3bb
kaanguru <kaanguru@mail.ru>
2022-08-11 19:57:02 +0300

    b

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <SlickList axis="y" v-model:list="fruits">
+    <SlickItem v-for="(fruit, i) in fruits" :key="fruit" :index="i">
+      {{ fruit }}
+    </SlickItem>
+  </SlickList>
+</template>
+
+<script lang="ts">
+import { SlickList, SlickItem } from 'vue-slicksort';
+
+export default {
+  components: {
+    SlickList,
+    SlickItem,
+  },
+  data() {
+    return {
+      fruits: ['Apples', 'Bananas' /* etc. */],
+    };
+  },
+};
+</script>
